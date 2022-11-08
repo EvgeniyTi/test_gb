e666610c5456dbf5f7e25d4a10f8edf658556d7f
etima <48822855+EvgeniyTi@users.noreply.github.com>
2022-11-08 18:32:53 +0300

    start programs: input array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+void Main(string args)
+{
+    string[] numbers = args.Split(',');
+
+    //int count = 0;
+    //for (int i = 0; i < numbers.Length; i++)
+    //{
+    //    if (Convert.ToInt64(numbers[i]) > 0) count++;
+    //}
+    //Console.WriteLine(String.Join(",", numbers));
+    //Console.Write($"{String.Join(" ", numbers).Trim()}");
+}
+
+Console.Write("Ввведите с клавиатуры массив строк: ");
+Main(args: Console.ReadLine());
