1cef312115cea0dc235f00c8834b2afd08a2b631
etima <48822855+EvgeniyTi@users.noreply.github.com>
2022-11-09 19:38:03 +0300

    final version

--- Program.cs
+++ Program.cs
@@ -1,17 +1,47 @@
 ﻿Console.Clear();
 
-void Main(string args)
+void Main()
 {
-    string[] numbers = args.Split(',');
-
-    //int count = 0;
-    //for (int i = 0; i < numbers.Length; i++)
-    //{
-    //    if (Convert.ToInt64(numbers[i]) > 0) count++;
-    //}
-    //Console.WriteLine(String.Join(",", numbers));
-    //Console.Write($"{String.Join(" ", numbers).Trim()}");
+    string[] userArray = InputItems();
+    string[] finalArray = FinalArray(userArray);
+    Console.Write($"[{String.Join(", ", userArray)}] -> [{String.Join(", ", finalArray)}]");
 }
 
-Console.Write("Ввведите с клавиатуры массив строк: ");
-Main(args: Console.ReadLine());
+string[] InputItems()
+{
+    Console.Write("Введите массив строк через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+string[] FinalArray(string[] array)
+{
+    string[] newArray = new string[CountItems(array)];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+
+        }
+    }
+
+    return newArray;
+}
+
+int CountItems(string[] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+Main();
